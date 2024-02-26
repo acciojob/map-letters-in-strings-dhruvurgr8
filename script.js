84f4e353423df675fc24461225074025d035ac63
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-26 13:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function mapLetters(word) {
+    const letterMap = {};
+
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+
+        
+        const symbolLetter = Symbol.for(letter);
+
+        if (!(symbolLetter in letterMap)) {
+            letterMap[symbolLetter] = [];
+        }
+
+        
+        letterMap[symbolLetter].push(i);
+    }
+
+    return letterMap;
+}
